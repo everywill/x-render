b388e2601bd47a79331725461f31c8964aeddaef
pan chen <panchen@pandeMacBook-Pro.local>
2023-12-01 12:20:48 +0800

    add comment

--- src/render-backend/graphics/buffer-desc.js
+++ src/render-backend/graphics/buffer-desc.js
@@ -1,7 +1,9 @@
+// taken from diligent, used in buffer creation
 const BUFFER_MODE = {
     BUFFER_MODE_UNDEFINED: 0,
-    BUFFER_MODE_FORMATTED: 1,
+    BUFFER_MODE_FORMATTED: 1,  // like 1D texture mostly
     BUFFER_MODE_STRUCTURED: 2,
-    BUFFER_MODE_RAW_VIEWS: 3,
+    BUFFER_MODE_RAW_VIEWS: 3,  // almost like formatted, but can be indexed in bytes
     BUFFER_MODE_NUM_MODES: 4,
 };
+
