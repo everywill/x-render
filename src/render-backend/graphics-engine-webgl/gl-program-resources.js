cbb28c916ad8d4f4e86423ace036049e2e37cc6e
everywill <everywillatchina@gmail.com>
2024-02-22 14:27:27 +0800

    update

--- src/render-backend/graphics-engine-webgl/gl-program-resources.js
+++ src/render-backend/graphics-engine-webgl/gl-program-resources.js
@@ -5,7 +5,8 @@ class GLProgramVariable {
     constructor(name, size, varType) {
         this.name = name;
         this.resources = [];
-        this.scale_uniforms = [];
+        // scale_uniform store data in uint8array
+        this.scale_uniform = null;
         this.array_size = size;
         this.var_type = varType;
     }
@@ -18,12 +19,27 @@ class UniformBufferInfo extends GLProgramVariable {
     }
 }
 
+// class StorageBlockInfo extends GLProgramVariable {
+//     constructor(name, size, varType, binding) {
+//         super(name, size, varType);
+//         this.binding = binding;
+//     }
+// }
+
 class SamplerInfo extends GLProgramVariable {
     constructor(name, size, varType, location, type, staticSampler) {
         super(name, size, varType);
         this.location = location;
         this.type = type;
         this.static_sampler = staticSampler;
+    }
+}
+
+class ImageInfo extends GLProgramVariable {
+    constructor(name, size, varType, binding, dataType) {
+        super(name, size, varType);
+        this.binding = binding;
+        this.type = dataType;
     }
 }
 
@@ -93,13 +109,95 @@ class ScaleUniformInfo extends GLProgramVariable {
         this.uniform_location = uniformLocation;
         this.scale_size = scaleSize;
         this.data_type = dataType;
+
+        let typeSize = 0;
+        const bytesInFloat = 4;
+        const bytesInInt32 = 4;
+        switch(this.data_type) {
+            case UNIFORM_TYPE.FLOAT:
+                typeSize = bytesInFloat * 1;
+                break;
+            case UNIFORM_TYPE.FLOAT2:
+                typeSize = bytesInFloat * 2;
+                break;
+            case UNIFORM_TYPE.FLOAT3:
+                typeSize = bytesInFloat * 3;
+                break;
+            case UNIFORM_TYPE.FLOAT4:
+                typeSize = bytesInFloat * 4;
+                break;
+            case UNIFORM_TYPE.MAT2X2:
+                typeSize = bytesInFloat * 4;
+                break;
+            case UNIFORM_TYPE.MAT3X3:
+                typeSize = bytesInFloat * 9;
+                break;
+            case UNIFORM_TYPE.MAT4X4:
+                typeSize = bytesInFloat * 16;
+                break;
+            case UNIFORM_TYPE.MAT2X3:
+            case UNIFORM_TYPE.MAT3X2:
+                typeSize = bytesInFloat * 6;
+                break;
+            case UNIFORM_TYPE.MAT2X4:
+            case UNIFORM_TYPE.MAT4X2:
+                typeSize = bytesInFloat * 8;
+                break;
+            case UNIFORM_TYPE.MAT3X4:
+            case UNIFORM_TYPE.MAT4X3:
+                typeSize = bytesInFloat * 12;
+                break;
+            case UNIFORM_TYPE.INT:
+                typeSize = bytesInInt32 * 1;
+                break;
+            case UNIFORM_TYPE.INT2:
+                typeSize = bytesInInt32 * 2;
+                break;
+            case UNIFORM_TYPE.INT3:
+                typeSize = bytesInInt32 * 3;
+                break;
+            case UNIFORM_TYPE.INT4:
+                typeSize = bytesInInt32 * 4;
+                break;
+            case UNIFORM_TYPE.UINT:
+                typeSize = bytesInInt32 * 1;
+                break;
+            case UNIFORM_TYPE.UINT2:
+                typeSize = bytesInInt32 * 2;
+                break;
+            case UNIFORM_TYPE.UINT3:
+                typeSize = bytesInInt32 * 3;
+                break;
+            case UNIFORM_TYPE.UINT4:
+                typeSize = bytesInInt32 * 4;
+                break;
+            default:
+                typeSize = 0;
+                break;
+        }
+        this.scale_uniform = new Uint8Array(typeSize);
     }
 }
 
 
 
 class GLProgramResources {
-    constructor() {}
+    constructor() {
+        // array of UniformBufferInfo
+        this.uniform_blocks = [];
+        // array of SamplerInfo
+        this.samplers = [];
+        // array of ScaleUniformInfo
+        this.scale_uniform_info = [];
+    }
+
+    GetUniformBlocks() { return this.uniform_blocks; }
+    GetSamplers() { return this.samplers; }
+    GetScaleUniforms() { return this.scale_uniform_info; }
+
+    LoadUniforms(renderDevice, glProgram, defaultVariableType, variableDescs, staticSamples) {}
+
+    GetShaderVariable(name) {}
 }
 
 export {
